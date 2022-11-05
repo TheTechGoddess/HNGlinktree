70a01415736ccb8b7b1252ff013ef4f8d29e3240
Favour-Cell <favourenwonwukc@gmail.com>
2022-11-05 23:19:08 +0100

    hash router added 3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Profile from './components/Profile';
+import Linktree from './components/Linktree';
 import App from './App';
+import Footer from './components/Footer';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Contact from './components/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <>
+    <Profile />
+    <Linktree />
+    <Footer />
+    </>
+  },
+  {
+    path: "/contact",
+    element: <>
+    <Contact />
+    <Footer />
+    </>
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
